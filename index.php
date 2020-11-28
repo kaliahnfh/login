1c75490ce2acbb8a0ee2fddd0e4dd7f65af9a712
kaliahnfh <kaliahanifah.nasution2003@gmail.com>
2020-11-28 16:31:42 +0700

    Membuat textbox username, password dan button

--- index.php
+++ index.php
@@ -9,10 +9,9 @@
 	<div id="main">
 		<h1>SIMPLE LOGIN</h1>
 		<form method="POST">
-			username <input type="text" name="username" class="text" autocomplete="off"
-			required>
-			password <input type="password" name="password" class="text" required>
-			<input type="submit" name="submit" id="sub">
+			<input type="text" name="username" class="text" autocomplete="off" placeholder="type username" required>
+			<input type="password" name="password" class="text" required placeholder="input password">
+			<input type="submit" name="submit" class="btn" value="Login">
 		</form>
 	</div>
 
