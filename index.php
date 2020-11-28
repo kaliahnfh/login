1ed4dcae132a2302330d37c38eec50e8cbe25999
kaliahnfh <kaliahanifah.nasution2003@gmail.com>
2020-11-28 15:26:07 +0700

    Menambahkan judul, username, dan password

--- index.php
+++ index.php
@@ -6,5 +6,15 @@
 </head>
 <body>
 
+	<div id="main">
+		<h1>SIMPLE LOGIN</h1>
+		<form method="POST">
+			username <input type="text" name="username" class="text" autocomplete="off"
+			required>
+			password <input type="password" name="password" class="text" required>
+			<input type="submit" name="submit" id="sub">
+		</form>
+	</div>
+
 </body>
 </html>
